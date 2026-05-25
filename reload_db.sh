3b284e4ef346a6e68a0cd83f07b2c71e5784689b
PHRupp <pat.h.rupp@gmail.com>
2026-05-25 11:08:09 -0500

    update data

--- reload_db.sh
+++ reload_db.sh
@@ -9,8 +9,8 @@ rm -rf $DATA_DIR/$DB_FILE
 
 # Running load_db.py with CSV files from the DATA_DIR
 python load_db.py \
-    --customers="$DATA_DIR/CC-Customers_2022-12-07-2026-05-01.csv" \
-    --orders="$DATA_DIR/CC-Orders-07122022-01052026.csv" \
+    --customers="$DATA_DIR/CC-Customers_2022-12-08-2026-05-25.csv" \
+    --orders="$DATA_DIR/CC-Orders-08122022-25052026.csv" \
     --old_pos_orders="$DATA_DIR/HC_OldPOS_Orders_final.csv" \
-    --items="$DATA_DIR/CC-Items-Sales_2022-12-07-2026-05-01.csv" \
+    --items="$DATA_DIR/CC-Items-Sales_2022-12-08-2026-05-25.csv" \
     --database="$DATA_DIR/$DB_FILE"
